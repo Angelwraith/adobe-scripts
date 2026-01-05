6c6f37c994d6cb5ce2faf8ca56a0afbec1289567
Angelwraith <angelwraith@gmail.com>
2026-01-05 15:53:45 -0500

    Update File Check.js

--- File Check.js
+++ File Check.js
@@ -3,7 +3,7 @@
 /*@METADATA{
   "name": "File Check",
   "description": "Production file readiness analysis",
-  "version": "2.8",
+  "version": "2.9",
   "target": "illustrator",
   "tags": ["file", "check", "report"]
 }@END_METADATA*/
